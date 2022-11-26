6a7fd241384182915750477c0946211b58caff5d
infurgigh <infurgigh@mail.ru>
2022-11-26 13:44:55 +0300

    First commit Sem2HomeTask1

--- Sem2Task1.py
+++ Sem2Task1.py
@@ -1,3 +1,14 @@
 # Напишите программу которая будет принимать на вход дробь
 # и показывать первую цифру бробной части
 
+decimal = (input('Введите дробное число: '))
+if ',' in decimal:
+    decimal_part = decimal.split(',')[1]
+    first_digit = decimal_part[:1]
+    print(first_digit)
+elif '.' in decimal:
+    decimal_part = decimal.split('.')[1]
+    first_digit = decimal_part[:1]
+    print(first_digit)
+else:
+    print ('Число не дробное')
